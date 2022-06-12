683bbd35a4a2acf2f9b166fb54b207c73b750e42
bcjat <bc.jat11@gmail.com>
2022-06-12 18:31:35 +0530

    closes #3 updated as required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { DayAgoPipe } from './pipes/day-ago.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DayAgoPipe],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pipes/day-ago.pipe.ts
@@ -0,0 +1,32 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dayAgo',
+})
+export class DayAgoPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    if (value) {
+      const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+      const intervals: { [key: string]: number } = {
+        day: 86400,
+      };
+      let counter;
+      for (const i in intervals) {
+        counter = Math.floor(seconds / intervals[i]);
+        if (counter > 6) {
+          let d = new Date(value);
+          return d.getDate() + '-' + d.getMonth() + '-' + d.getFullYear();
+        } else if (counter > 0) {
+          if (counter === 1) {
+            return counter + ' ' + i + ' ago'; // singular (1 day ago)
+          } else {
+            return counter + ' ' + i + 's ago'; // plural (2 days ago)
+          }
+        } else {
+          return ' ' + 'today';
+        }
+      }
+    }
+    return value;
+  }
+}
